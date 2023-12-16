3bf36b86358376901f372f3222b8d48dd3326589
Vish Ravichandran <vishravi03@gmail.com>
2023-12-15 19:46:01 -0500

    Final commit

--- shell.c
+++ shell.c
@@ -23,19 +23,15 @@ const char *PATH_SEPARATOR = ":";
 
 // --------------------------------------------
 // Currently only two builtin commands for this
-// assignment exist
 // --------------------------------------------
 const char* BUILT_IN_COMMANDS[] = { "cd", "exit", NULL };
 
 
 /* ------------------------------------------------------------------------------
  *
- * YOU NEED TO COMPLETE THIS FUNCTION.
+ * Allocate memory for the command. 
  *
- * Allocate memory for the command. The last element in `p_cmd->argv` should be
- * NULL.
- *
- * The length of `p_cmd->argv` should be `argc+1`: the first `argc` slots are
+ * The length of `p_cmd->argv` is `argc+1`: the first `argc` slots are
  * used to store the arguments and the last one is set to NULL.
  *
  * Arguments:
@@ -56,13 +52,8 @@ void alloc_mem_for_command(command_t* p_cmd, int argc) {
 
 /* ------------------------------------------------------------------------------
  *
- * YOU NEED TO COMPLETE THIS FUNCTION.
- *
  * This function is used to free memory that may be malloc'd for the name and
- * argv fields in the command_t structure.  To be safe, you should also set the
- * pointer values to NULL.
- *
- * HINT(s): See man page for more information about free function
+ * argv fields in the command_t structure.
  *
  * Arguments:
  *         p_cmd : pointer to a command_t structure
@@ -81,41 +72,8 @@ void cleanup(command_t* p_cmd) {
 }
 /* ------------------------------------------------------------------------------
  *
- * YOU NEED TO COMPLETE THIS FUNCTION.
- *
- * This function will parse the command (cmd for short) and its arguments
+ * This function parses the command and its arguments
  * specified by the user.
- *
- * HINT(s): This function is "essentially" an exercise in string parsing.
- *
- *         EXAMPLE#1, if the user enters
- *
- *                                 cd /mnt/cdrom
- *
- *                 at the shell prompt, line would contain "cd /mnt/cdrom".
- *                 Using the space character as the delimiter, the fields in
- *                 the command struct would be:
- *
- *                                 - argc = 2
- *                                 - argv = {"cd", "/mnt/cdrom", NULL}
- *
- *         EXAMPLE#2, if the user enters
- *
- *                                 ls -la
- *
- *                 at the shell prompt, line would contain "ls -la". Using the
- *                 space character as the delimiter, the fields in the command
- *                 struct would be:
- *
- *                                 - argc = 2
- *                                 - argv = {"ls", "-la", NULL}
- *
- *         EXAMPLE#3, if the user enters nothing at the shell prompt (i.e.
- *         simply hits the return key), line would NULL, and the fields in
- *         the command struct would be:
- *
- *                                 - argc = 0
- *                                 - argv = {NULL}
  *
  *  Arguments:
  *      line: pointer to the string containing the cmd.
@@ -152,34 +110,11 @@ void parse(char* line, command_t* p_cmd) {
 
 /* ------------------------------------------------------------------------------
  *
- * YOU NEED TO COMPLETE THIS FUNCTION.
- *
- * This function is used determine if the named command (cmd for short) entered
+ * This function is determines if the named command entered
  * by the user in the shell can be found in one of the folders defined in the
- * PATH environment (env or short) variable. If the file exists, then the name
+ * PATH environment. If the file exists, then the name
  * of the executable at argv[0] is replaced with the location (fully qualified
  * path) of the executable.
- *
- * For example,
- *
- * if command_t.argv[0] is "ls", and "ls" is in the "/usr/bin" folder,
- * then command_t.argv[0] would be changed to "/usr/bin/ls"
- *
- * HINT(s): Use getenv() system function to retrieve the folders defined in the
- *                 PATH variable. An small code segment is shown below that
- * demonstrates how to retrieve folders defined in your PATH.
- *
- *                         char* path_env_variable;
- *                         path_env_variable = getenv( "PATH" );
- *                         printf("PATH = %s\n", path_env_variable );
- *
- *                 The format of the PATH is very simple, the ':' character is
- * delimiter, i.e. used to mark the beginning and end of a folder defined in the
- * path.
- *
- *                 Write a loop that parses each folder defined in the path,
- * then uses this folder along with the stat function (see "File/Directory
- * Status" section in the assignment PDF).
  *
  *  Arguments:
  *         p_cmd: pointer to the command_t structure
@@ -216,10 +151,7 @@ bool find_fullpath(command_t* p_cmd) {
 
 /* ------------------------------------------------------------------------------
  *
- * YOU NEED TO COMPLETE THIS FUNCTION.
- *
- * This function will execute external commands (cmd for short).
- *
+ * This function will execute external commands.
  *
  * Arguments:
  *      p_cmd: pointer to the command_t structor
@@ -294,8 +226,6 @@ bool is_builtin(command_t* p_cmd) {
 /* ------------------------------------------------------------------------------
  *
  * This function is used execute built-in commands such as change directory (cd)
- *
- * HINT(s): See man page for more information about chdir function
  *
  * Arguments:
  *      p_cmd: pointer to the command_t structure
